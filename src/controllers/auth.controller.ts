19b7e0d22933e9fbc71d6dc2dfc423b81094fb47
atillatrisakti <atillasilvaaa@gmail.com>
2025-07-29 00:37:54 +0700

    feat: setup auth register

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import * as Yup from "yup";
+
+type TRegister = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const registerValidationSchema = Yup.object({
+  fullName: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), ""], "Passwords not match!"),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    const { fullName, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+    try {
+      await registerValidationSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+      res.status(200).json({
+        message: "Registration successful",
+        data: {
+          fullName,
+          username,
+          email,
+        },
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
