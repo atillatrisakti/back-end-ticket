a0ecb2168f9f6f652931556585f7670d56780455
atillatrisakti <atillasilvaaa@gmail.com>
2025-08-08 17:48:19 +0700

    feat: login & get user endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import * as Yup from "yup";
 
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
 
 type TRegister = {
   fullName: string;
@@ -9,6 +12,11 @@ type TRegister = {
   email: string;
   password: string;
   confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
 };
 
 const registerValidationSchema = Yup.object({
@@ -43,6 +51,64 @@ export default {
 
       res.status(200).json({
         message: "Registration successful",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      const userByIdentifier = await UserModel.findOne({
+        $or: [{ email: identifier }, { username: identifier }],
+      });
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "Login Successful",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "Success getting user profile",
         data: result,
       });
     } catch (error) {
